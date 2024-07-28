833aa3ae677b9bf703f2a24cd9e5436d0474e16d
Polaki Gunakar <131868369+Gunakar-Polaki@users.noreply.github.com>
2024-07-29 01:06:28 +0530

    Update movie.py

--- movie.py
+++ movie.py
@@ -59,7 +59,7 @@ st.title("Movie Recommendation System")
 # Movie Selection
 movie_titles = dict(zip(movies['movieId'], movies['title']))
 movie_options = list(movie_titles.values())
-selected_movie_title = st.selectbox("Select a movie:", movie_options)
+selected_movie_title = st.selectbox("Select a movie:", movie_options, index=None)
 
 if selected_movie_title:
     selected_movie_id = movies[movies['title'] == selected_movie_title]['movieId'].values[0]
